8e270e3c71137a7695d1a5d9b4b936d9364e71ab
Marek Rychlik <rychlik@arizona.edu>
2024-09-26 09:45:55 -0700

    *** empty log message ***

--- Method1/ctypes_2d_array.py
+++ Method1/ctypes_2d_array.py
@@ -12,10 +12,10 @@ flat_array_2d = [item for row in array_2d for item in row]
 c_array_2d = (ctypes.c_int * len(flat_array_2d))(*flat_array_2d)
 
 # Define the function signature
-lib.modify_2d_array.argtypes = (ctypes.c_int, ctypes.POINTER(ctypes.c_int))
+lib.modify_2d_array.argtypes = (ctypes.POINTER(ctypes.c_int), ctypes.c_int, ctypes.c_int, ctypes.c_int)
 
 # Modify the array in C (e.g., add 10 to each element)
-lib.lu_in_place(c_array_2d, rows, cols, 10)
+lib.modify_2d_array(c_array_2d, rows, cols, 10)
 
 # Convert back to a 2D Python list
 modified_array_2d = [
