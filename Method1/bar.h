5dd90096cde2a6e9f8297e9ae35dcee24ce60db5
Marek Rychlik <rychlik@arizona.edu>
2025-11-26 10:47:50 -0700

    *** empty log message ***

---
+++ Method1/bar.h
@@ -0,0 +1,11 @@
+#ifndef BAR_H
+#define BAR_H
+
+void print_1d_array(int* array, int length);
+int bar_add(int x, int y);
+void print_2d_array(int* array, int rows, int cols);
+void modify_1d_array(int* array, int length);
+void modify_2d_array(int* array, int rows, int cols, int value);
+
+
+#endif	/* BAR_H */
