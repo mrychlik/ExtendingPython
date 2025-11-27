a50ecb37060482a08cd541538e72d57a202d774b
Marek Rychlik <rychlik@arizona.edu>
2025-11-26 17:11:58 -0700

    *** empty log message ***

--- Method1/bar.h
+++ Method1/bar.h
@@ -1,3 +1,20 @@
+/*----------------------------------------------------------------
+* File:     bar.h
+*----------------------------------------------------------------
+*
+* Author:   Marek Rychlik (rychlik@arizona.edu)
+* Date:     Wed Nov 26 17:07:51 2025
+* Copying:  (C) Marek Rychlik, 2020. All rights reserved.
+*
+*----------------------------------------------------------------*/
+/* The header file is used to declare "exported" functions to
+ * other C code. It is used to create the library libbar.so.
+ * It is NOT used by Python code CALLING the functions declared
+ * In this file: Python must use its own mechanism to inform
+ * the 'ctypes' or 'cffi' module about the signatures of
+ * the functions (argument types, return type).
+ */
+
 #ifndef BAR_H
 #define BAR_H
 
